7deac843055dcf561c9c0f5a46dab3d6e33c5bd8
Bagas-Pradana <bagaspradana599@gmail.com.com>
2025-02-09 16:23:35 +0700

    update data bagas

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,12 @@
+@include('partials.header')
+{{-- Navigation Panel --}}
+<div class="bg-gray-100 flex flex-wrap items-center justify-center px-8 w-full">
+    <h1 class="text-4xl font-bold font-serif text-cyan-500 w-1/2 text-left justify-self-start">Bagas</h1>
+    <div class="w-1/2 flex justify-evenly gap-x-8">
+        <span class="text-blue-400 text-2xl font-bold font-sans">Home</span>
+        <span class="text-blue-400 text-2xl font-bold font-sans">About</span>
+        <span class="text-blue-400 text-2xl font-bold font-sans">Blog</span>
+    </div>
+</div>
+
+@include('partials.footer')
---
+++ resources/views/testing.blade.php
@@ -0,0 +1,3 @@
+@include('partials.navbar')
+{{-- <h1 class="text-5xl font-bold text-orange-300">Hellow World</h1> --}}
+{{-- @include('partials.footer') --}}
